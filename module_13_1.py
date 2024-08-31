6b5c6614edd472cba7ba0a5a97b3954de8183ea5
Vasiliy <v_derkach@inbox.ru>
2024-08-31 09:22:19 +0500

    2

--- module_13_1.py
+++ module_13_1.py
@@ -2,7 +2,7 @@ import asyncio
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for i in range(5):
-        asyncio.sleep(20//power)
+        await asyncio.sleep(20//power)
         print(f'Силач {name} поднял {1}' )
     print(f'Силач {name} закончил соревнования.')
 async def start_tournament():
