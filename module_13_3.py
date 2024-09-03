17e381db0b8b10fe0143ecf6716172906092fbcd
1 <v_derkach@inbox.ru>
2024-09-03 09:00:43 +0500

    03/09/24

---
+++ module_13_3.py
@@ -0,0 +1,30 @@
+import os
+import re
+from aiogram import  Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+with open('conf_both.cnf', 'r') as cnf:
+    ctxt = cnf.read()
+    po = re.search('(?<=' + 'bot_kye=' + ').*?(?=\n)', ctxt)
+    key_bot_api = po.group(0)
+    # print(key_bot)
+bot = Bot(token=key_bot_api)
+disp = Dispatcher(bot, storage=MemoryStorage())
+
+@disp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+
+@disp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+
+if __name__=='__main__':
+    executor.start_polling(disp,skip_updates=True)
+
+
